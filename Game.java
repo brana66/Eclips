84d71c19d7abe10320f5d3f025727792e6e5483e
imbu3 <burana612000@gmail.com>
2021-10-20 15:45:12 +0530

    Game v1

---
+++ Game.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Game {
+    public static void main(String[]args){
+    
+    	System.out.println("Let's play rock, paper, scissor");
+      
+    	System.out.println("Enter 0 : rock | 1 : paper | 2 : scissor" +"\n" + "enter your choice : ");
+        Scanner sc = new Scanner(System.in);
+        int ui = sc.nextInt();
+
+        Random rd = new Random();
+        int ci = rd.nextInt(3);
+
+        System.out.println("ci choice: " + ci);
+
+        if(ui==ci){
+            System.out.println("Draw"); }
+        else if ( ui==0 && ci==1 || ui==1 && ci==2 || ui==2 && ci==0){
+            System.out.println("ci wins");}
+        else
+            System.out.println("ui wins");
+
+    }
+}
