19dfaea445ca8dc5138012911ec2b279964620e3
imbu3 <burana612000@gmail.com>
2021-10-20 15:40:53 +0530

    Emoji added

--- Hello.java
+++ Hello.java
@@ -1,4 +1,14 @@
+import java.util.Scanner;
+
 
 public class Hello {
+
+	public static void main(String[] args) {
+		System.out.print("Enter name: ");
+		Scanner sc = new Scanner(System.in);
+				String s = sc.nextLine();
+		System.out.println(s+","+" " + "Welcome to the App!");
+		System.out.println(":)");
+	}
 
 }
